c49e8882946b798c25fddc7d4a3ddb7f89e17b85
TheHinoto <david@deschanel.fr>
2021-12-02 15:41:17 +0100

    Update main.py

--- day-2/main.py
+++ day-2/main.py
@@ -2,29 +2,31 @@ from pathlib import Path
 
 import Submariner1, Submariner2
     
-submariner1 = Submariner1.Submariner()
-submariner2 = Submariner2.Submariner()
+if __name__ == "__main__":
 
-moove_file = open(Path(__file__).with_name('mooves.txt'))
-moove_list = moove_file.readlines()
+    submariner1 = Submariner1.Submariner()
+    submariner2 = Submariner2.Submariner()
 
-for instruction in moove_list:
-    move = instruction.rstrip('\n')
-    [direction, distance] = move.split(" ")
-    distance = int(distance)
-    
-    if (direction == "up"):
-        submariner1.move_upward(distance)
-        submariner2.move_upward(distance)
-    
-    elif (direction == "down"):
-        submariner1.move_downward(distance)
-        submariner2.move_downward(distance)
+    moove_file = open(Path(__file__).with_name('mooves.txt'))
+    moove_list = moove_file.readlines()
+
+    for instruction in moove_list:
+        move = instruction.rstrip('\n')
+        [direction, distance] = move.split(" ")
+        distance = int(distance)
         
-    elif (direction == "forward"):
-        submariner1.move_forward(distance)
-        submariner2.move_forward(distance)
+        if (direction == "up"):
+            submariner1.move_upward(distance)
+            submariner2.move_upward(distance)
         
-    
-print(f"Awnser 1 : {submariner1.get_h_pos()*submariner1.get_v_pos()}")
-print(f"Awnser 2 : {submariner2.get_h_pos()*submariner2.get_v_pos()}")
+        elif (direction == "down"):
+            submariner1.move_downward(distance)
+            submariner2.move_downward(distance)
+            
+        elif (direction == "forward"):
+            submariner1.move_forward(distance)
+            submariner2.move_forward(distance)
+            
+        
+    print(f"Awnser 1 : {submariner1.get_h_pos()*submariner1.get_v_pos()}")
+    print(f"Awnser 2 : {submariner2.get_h_pos()*submariner2.get_v_pos()}")
