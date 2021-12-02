4abce8f4ef16538409bad1816f0bd6f55164fbe8
TheHinoto <david@deschanel.fr>
2021-12-02 12:17:05 +0100

    finished day 1

--- day-1/main.py
+++ day-1/main.py
@@ -1,1 +1,42 @@
 import os
+
+deph_file = open('numbers.txt')
+deph_flat_list = deph_file.readlines()
+
+depth_array = []
+
+for deep_indice in deph_flat_list:
+    depth_array.append(int(deep_indice.rstrip('\n')))
+    
+"""
+Premier excercise
+"""
+
+last_depth = None
+nb_last_depth_dec = 0
+
+for current_depth in depth_array:
+    if (last_depth):
+        if (last_depth < current_depth):
+            nb_last_depth_dec +=1
+    last_depth = current_depth
+    
+print (f"Reponse 1 : {nb_last_depth_dec}")
+    
+"""
+Second excercise
+"""
+depht_sliding_window = []
+depth_last_sum = 0
+depth_increase_time = 0
+
+for current_depth in depth_array:
+    depht_sliding_window.append(current_depth)
+    if (len(depht_sliding_window)==3):
+        if(depth_last_sum):
+            if(sum(depht_sliding_window) > depth_last_sum):
+                depth_increase_time +=1
+        depth_last_sum = sum(depht_sliding_window)
+        depht_sliding_window.pop(0)
+        
+print (f"Reponse 2 : {depth_increase_time}")
