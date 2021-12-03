3544c23e22da340a5163b5d7a813ad98b9be9cd9
TheHinoto <david@deschanel.fr>
2021-12-03 12:45:44 +0100

    day 3 - Ex1 finished

--- 2021/day-3/main.py
+++ 2021/day-3/main.py
@@ -1,2 +1,60 @@
-if __name__ == "__main__":
-    print("Hello World")
+from os import system
+from pathlib import Path
+from time import sleep
+
+def convert_binnary_array_to_integer(binnary_array: list):
+    binnary_array.reverse()
+    binnary_value=0
+    for i in range(len(binnary_array)):
+        binnary_value += binnary_array[i]*(2**(i))
+    return binnary_value
+    
+with open(Path(__file__).with_name('input.txt'),"r") as f:
+    inputs = f.readlines()
+
+diag_array = []
+diag_bit_array = []
+
+for el in inputs:
+    el = el.rstrip('\n')
+    for diag_bit in el:
+        diag_bit_array.append(int(diag_bit))
+    diag_array.append(diag_bit_array.copy())
+    diag_bit_array.clear()
+
+"""
+Excercice One
+"""   
+
+bit_analysed_pos = 0
+bit_analysed_pos_maxvalue = len(diag_array[0])
+diag_bit_array_size = len(diag_array)
+
+count_bit_one = count_bit_zero = 0
+gamma_rate = []
+epsilon_rate = []
+
+for i in range (bit_analysed_pos_maxvalue):
+    for j in range(diag_bit_array_size):
+        if(diag_array[j][i]):
+            count_bit_one += 1
+        else:
+            count_bit_zero += 1
+    if count_bit_one > count_bit_zero:
+        gamma_rate.append(1)
+        epsilon_rate.append(0)     
+    else:
+        gamma_rate.append(0)
+        epsilon_rate.append(1)
+        
+    count_bit_one = count_bit_zero = 0
+    print(gamma_rate)
+    print(epsilon_rate)
+    print("\n")
+
+gamma_rate_decimal = convert_binnary_array_to_integer(gamma_rate)
+epsilon_rate_decimal = convert_binnary_array_to_integer(epsilon_rate)
+
+print(f"Gamma Rate : {gamma_rate_decimal}")
+print(f"Epsilon Rate : {epsilon_rate_decimal}")
+print(f"Power Consumtion : {gamma_rate_decimal*epsilon_rate_decimal}")
